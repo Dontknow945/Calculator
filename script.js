6d2e74481c35be5b313897022548e3d6da9e6a06
dontknow945 <a0978878897@gmail.com>
2021-12-15 20:58:04 +0800

    add display and buttons

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const display = document.querySelector('.display');
+const addBtn = document.querySelector('.add');
+const subBtn = document.querySelector('.subtract');
+const mulBtn = document.querySelector('.multiply');
+const divBtn = document.querySelector('.divide');
+const equBtn = document.querySelector('.equals');
+const pointBtn = document.querySelector('.point');
+const zeroBtn = document.querySelector('.zero');
+const oneBtn = document.querySelector('.one');
+const twoBtn = document.querySelector('.two');
+const threeBtn = document.querySelector('.three');
+const fourBtn = document.querySelector('.four');
+const fiveBtn = document.querySelector('.five');
+const sixBtn = document.querySelector('.six');
+const sevenBtn = document.querySelector('.seven');
+const eightBtn = document.querySelector('.eight');
+const nineBtn = document.querySelector('.nine');
+
+let solution = 0;
+
 function add(a, b) {
     return a + b;
 }
